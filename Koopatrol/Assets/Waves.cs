14c24276921c4fcd3cc5e0f4e31e6519aabc2b86
Dhranios <dhranios@gmail.com>
2021-10-04 11:54:42 +0200

    Round counter

--- Koopatrol/Assets/Waves.cs
+++ Koopatrol/Assets/Waves.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Waves : MonoBehaviour
 {
@@ -14,17 +15,18 @@ public class Waves : MonoBehaviour
 
 
     public int[] Amountspawntimes;
-    public int wavecount;
 
     public int enemiesWaveIndex;
     public int waveIndex;
 
     public float waveDelay;
     public GameObject SpawnEnemies;
+    GameObject RoundCounter;
     // Start is called before the first frame update
     void Start()
     {
-
+        RoundCounter = GameObject.FindGameObjectWithTag("RoundCounter");
+        RoundCounter.GetComponent<Text>().text = "Round: " + (waveIndex + 1);
     }
 
     // Update is called once per frame
@@ -34,12 +36,13 @@ public class Waves : MonoBehaviour
         if (enemiesWaveIndex == TheWaves[waveIndex].wave.Count)
         {    
             timeBetweenWaves();
-            if(waveDelay == 0){
-            SpawnEnemies.GetComponent<SpawnEnemies>().stopSpawning = false;
-            waveIndex++;
-            enemiesWaveIndex = 0;
-            Debug.Log(waveIndex);
-            waveDelay= 5;         
+            if (waveDelay == 0)
+            {
+                SpawnEnemies.GetComponent<SpawnEnemies>().stopSpawning = false;
+                waveIndex++;
+                RoundCounter.GetComponent<Text>().text = "Round: " + (waveIndex + 1);
+                enemiesWaveIndex = 0;
+                waveDelay = 5;
             }
         }
 
@@ -47,7 +50,6 @@ public class Waves : MonoBehaviour
 
     void timeBetweenWaves(){
         SpawnEnemies.GetComponent<SpawnEnemies>().stopSpawning = true;
-        Debug.Log(waveDelay);
         if(waveDelay > 0){
                 waveDelay -= Time.deltaTime;
         }
