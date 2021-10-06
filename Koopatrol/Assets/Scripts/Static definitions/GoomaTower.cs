1df6cd20bbcfe6d7f38e1a3022ca25e9f37163ab
Dhranios <dhranios@gmail.com>
2021-10-06 09:25:52 +0200

    Tower nerfing

--- Koopatrol/Assets/Scripts/Static definitions/GoomaTower.cs
+++ Koopatrol/Assets/Scripts/Static definitions/GoomaTower.cs
@@ -6,7 +6,7 @@ namespace Assets
     public static class GoomaTower
     {
         public static int bulletImage = 0;
-        public static float range = 400;
+        public static float range = 200;
         public static int cooldown = 2;
         public static int damage = 1;
         public static float speed = 50f;
