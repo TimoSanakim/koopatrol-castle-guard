d11f911197f60ae52662db650bf23f2c23052ce3
Timo <445650@student.fontys.nl>
2021-10-18 10:31:03 +0200

    added tutorial

---
+++ Koopatrol/Assets/Scripts/tutorial.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void tutorialscript(){
+        gameObject.SetActive(false);
+    }
+
+}
