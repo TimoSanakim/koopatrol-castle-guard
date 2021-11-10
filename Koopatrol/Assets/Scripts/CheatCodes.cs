96fe957b05059cad8b345e6acd795171c813db50
Dhranios <dhranios@gmail.com>
2021-11-10 11:40:32 +0100

    Remove cheat codes

--- Koopatrol/Assets/Scripts/CheatCodes.cs
+++ Koopatrol/Assets/Scripts/CheatCodes.cs
@@ -19,7 +19,7 @@ public class CheatCodes : MonoBehaviour
     void Update()
     {
         
-        if (Input.GetKeyDown(KeyCode.M))
+        /*if (Input.GetKeyDown(KeyCode.M))
         {
             Assets.CoinCounter.ChangeCoinCounter(50, true);
         }
@@ -37,7 +37,7 @@ public class CheatCodes : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.K))
         {
-            if (Map.Enemies[0] != null) Map.Enemies[0].GetComponent<EnemyHealth>().Hurt(100000);
-        }
+            if (Map.Enemies.Count != 0) Map.Enemies[0].GetComponent<EnemyHealth>().Hurt(100000);
+        }*/
     }
 }
